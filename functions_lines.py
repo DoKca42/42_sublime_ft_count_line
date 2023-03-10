512ab91b85410bab70b973f9e716bd1cf01be15c
DoKca <112852191+DoKca42@users.noreply.github.com>
2023-03-10 02:57:20 +0100

    0.2.4 - REGEX function
    REGEX function

--- functions_lines.py
+++ functions_lines.py
@@ -2,6 +2,33 @@ import sublime
 import sublime_plugin
 import re
 
+def RegexFunction(txt):
+	out = []
+	i = 0
+	while i < len(txt):
+		start = 0
+		temp = 0
+		parentheses = 0
+		while i < len(txt) and not txt[i].isalnum() and not txt[i] == '_':
+			i += 1
+		start = i
+		while i < len(txt) and (txt[i].isalnum() or txt[i] == '_'):
+			i += 1
+		if i < len(txt) and txt[i] == '(':
+			parentheses = 1
+			i += 1
+			temp = i
+			while i < len(txt) and parentheses > 0 and txt[i] != ';':
+				if txt[i] == '(':
+					parentheses += 1
+				if txt[i] == ')':
+					parentheses -= 1
+				i += 1
+			if i < len(txt) and parentheses == 0:
+				out.append((start, i, txt[start:i]))
+			i = temp
+	return out
+
 class FunctionLinesCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		txt = self.view.substr(sublime.Region(0, self.view.size()))
@@ -11,10 +38,11 @@ class FunctionLinesCommand(sublime_plugin.TextCommand):
 		while a <= len(lines):
 			self.view.erase_phantoms("42_function_lines_total")
 			a += 1
-		all_res_regex = re.finditer(r"[a-zA-Z_][a-zA-Z_0-9]*\((?:[^();]|\((?:[^();]+|\([^();]*\))*\))*(?:\)|(?=;))", txt)
+
+		all_res_regex = RegexFunction(txt)
 		for res_regex in all_res_regex:
-			if txt[res_regex.end()] == '\n' and txt[res_regex.end() + 1] == '{':
-				i = res_regex.end() + 2
+			if txt[res_regex[1]] == '\n' and txt[res_regex[1] + 1] == '{':
+				i = res_regex[1] + 2
 				brackets = 1
 				while brackets != 0 and i < len(txt):
 					if txt[i] == '{':
@@ -23,7 +51,7 @@ class FunctionLinesCommand(sublime_plugin.TextCommand):
 						brackets -= 1
 					i += 1
 				size = 0
-				start = res_regex.end() + 3
+				start = res_regex[1] + 3
 				end = i - 1
 				while start < end:
 					if txt[start] == '\n':
@@ -33,4 +61,4 @@ class FunctionLinesCommand(sublime_plugin.TextCommand):
 					html = '<body style="color:#bf2525;font-style:italic;margin-left:10px;">Function lines: ' + str(size) + '</body>'
 				else:
 					html = '<body style="color:#8d9090;font-style:italic;margin-left:10px;">Function lines: ' + str(size) + '</body>'
-				self.view.add_phantom("42_function_lines_total",sublime.Region(i - 1, self.view.size()), html, sublime.LAYOUT_BLOCK)
+				self.view.add_phantom("42_function_lines_total", sublime.Region(i - 1, self.view.size()), html, sublime.LAYOUT_BLOCK)
